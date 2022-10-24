07cb3104ee041ebfe255d91f10d3944b310c57d7
Andrew <chirkash11@gmail.com>
2022-10-24 22:15:38 +0300

    pt3ex2

--- src/Car.java
+++ src/Car.java
@@ -7,12 +7,38 @@ public class Car {
     private final String productionCountry;
 
     public Car(String brand, String model, String color, int productionYear, double engineVolume, String productionCountry) {
-        this.brand = brand;
-        this.color = color;
-        this.engineVolume = engineVolume;
-        this.model = model;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+        if (brand == null) {
+          this.brand = "default";
+        }else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        }else {
+            this.model = model;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        }else {
+            this.productionCountry = productionCountry;
+        }
+        if (color == null) {
+            this.color = "белый";
+        }else {
+            this.color = color;
+        }
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        }else {
+            this.productionYear = productionYear;
+        }
+        if (engineVolume <= 0){
+            this.engineVolume = 1.5;
+        }else {
+            this.engineVolume = engineVolume;
+        }
+
+
     }
     void carInfo(){
         System.out.println("Машина - " + brand + " " + model + ", " + productionYear + " года выпуска, страна производства " + productionCountry + ", цвет - " + color + ", объём двигателя  - " + engineVolume + " литров.");
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Car Lada = new Car("Lada", "Granta", "жёлтый", 2015, 1.7,"Россия");
-        Car Audi = new Car("Audi", "A8 50 L TDI quattro", "чёрный", 2020, 3.0,"Германия");
-        Car BMW = new Car("BMW", "Z8", "чёрный", 2021, 3.0,"Германия");
-        Car Kia = new Car("Kia", "Sportage 4-го поколения", "красный", 2018, 2.4,"Южная Корея");
-        Car Hyundai = new Car("Hyundai", "Avante", "оранжевый", 2016, 1.6,"Южная Корея");
-        Lada.carInfo();
-        Audi.carInfo();
-        BMW.carInfo();
-        Kia.carInfo();
-        Hyundai.carInfo();
+        Car LadaGranta = new Car("Lada", "Granta", "жёлтый", 2015, 1.7,"Россия");
+        Car AudiA8 = new Car("Audi", "A8 50 L TDI quattro", "чёрный", 2020, 3.0,"Германия");
+        Car BMWz8 = new Car("BMW", "Z8", "чёрный", 2021, 3.0,"Германия");
+        Car KiaSportage4 = new Car("Kia", "Sportage 4-го поколения", "красный", 2018, 2.4,"Южная Корея");
+        Car HyundaiAvante = new Car("Hyundai", "Avante", "оранжевый", 2016, 1.6,"Южная Корея");
+        LadaGranta.carInfo();
+        AudiA8.carInfo();
+        BMWz8.carInfo();
+        KiaSportage4.carInfo();
+        HyundaiAvante.carInfo();
 
     }
 }
