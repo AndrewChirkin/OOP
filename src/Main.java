3f8c4bf270989ac143e03c4bd663d483e993e73b
Andrew <chirkash11@gmail.com>
2022-10-24 20:38:21 +0300

    pt1ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Human Maxim = new Human("Максим", "Минск", 1988);
+    Human Anna = new Human("Аня", "Москва", 1993);
+    Human Kate = new Human("Катя", "Калининград", 1992);
+    Human Artem = new Human("Артём", "Москва", 1995);
+        Maxim.greeting();
+        Anna.greeting();
+        Kate.greeting();
+        Artem.greeting();
     }
 }
